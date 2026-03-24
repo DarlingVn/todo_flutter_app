84ba606dffc366285bb0855a2dbc7ce5c4e56eaf
DarlingVn <146715178+DarlingVn@users.noreply.github.com>
2026-03-24 15:21:31 +0700

    update

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -42,7 +42,6 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     _tabController.dispose();
     super.dispose();
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
